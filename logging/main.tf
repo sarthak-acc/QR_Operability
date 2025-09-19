e3b7666ce1c76c9afe8f5d760069ddefd92a5739
Rory Nguyen <rory.nguyen@oracle.com>
2025-09-19 12:19:39 -0700

    format

--- logging/main.tf
+++ logging/main.tf
@@ -18,7 +18,7 @@ resource "oci_log_analytics_log_analytics_log_group" "these" {
   namespace      = data.oci_log_analytics_namespaces.logging_analytics_namespaces.namespace_collection[0].items[0].namespace
   defined_tags   = each.value.defined_tags != null ? each.value.defined_tags : var.logging_configuration.default_defined_tags
   freeform_tags  = merge(local.cislz_module_tag, each.value.freeform_tags != null ? each.value.freeform_tags : var.logging_configuration.default_freeform_tags)
-  depends_on = [time_sleep.log_group_propagation_delay]
+  depends_on     = [time_sleep.log_group_propagation_delay]
 }
 
 resource "oci_logging_log" "these" {
